85683931ff0d578bbf791a94a48fb1a7f0c6c3d6
smartcloudi <pierrick35+smartcloudi@gmail.com>
2019-07-26 11:16:01 -0400

    Use cognito authentication on the backend

--- material/dist/js/auth.js
+++ material/dist/js/auth.js
@@ -2,6 +2,7 @@ var cognitoUser = undefined;
 var currentSession = undefined;
 var accessToken = undefined;
 
+const authApiBasePath = "https://ommkdunauc.execute-api.eu-central-1.amazonaws.com/dev";
 const categoriesApiBasePath = "https://g8335hfcy0.execute-api.eu-central-1.amazonaws.com/dev";
 const locationsApiBasePath = "https://6mo2qzbcag.execute-api.eu-central-1.amazonaws.com/dev"
 const providersApiBasePath = "https://sclx4kb3lk.execute-api.eu-central-1.amazonaws.com/dev";
@@ -10,35 +11,61 @@ const quotesApiBasePath = "https://z75j3glj94.execute-api.eu-central-1.amazonaws
 const usersApiBasePath = "https://ommkdunauc.execute-api.eu-central-1.amazonaws.com/dev";
 
 function isLoggedIn() {
-    var data = {
-        UserPoolId: 'eu-central-1_7iruUqdsk',
-        ClientId: '3gbfhl35avtb3uop6o5scbd0sd'
-    };
-    var userPool = new AmazonCognitoIdentity.CognitoUserPool(data);
-    cognitoUser = userPool.getCurrentUser();
 
-    if (cognitoUser != null) {
-        cognitoUser.getSession(function (err, session) {
-            if (err || !session.isValid()) {
-                $(location).attr("href", "pages-login.html");
-                return;
-            }
-            console.log('session validity: ' + session.isValid());
-            console.log("Session:", session);
-            currentSession = session;
-            accessToken = session.idToken.jwtToken;
-            console.log("Access token:", accessToken);
-        });
-    } else {
+    // Check if token is still valid
+    var token;
+
+    try {
+        token = JSON.parse(localStorage.getItem("idToken")).jwtToken;
+    } catch (error) {
+        console.error(error);
         $(location).attr("href", "pages-login.html");
     }
+    $.ajax({
+        method: "POST",
+        url: `${authApiBasePath}/verifyToken`,
+        contentType: "application/json",
+        dataType: "json",
+        cache: false,
+        data: JSON.stringify({
+            token: token
+        })
+    })
+        .done((data, textStatus, jqXHR) => {
+            console.log("Text Status:", textStatus);
+            console.log("Data:", data);
+            console.log("jqXHR:", jqXHR);
+            // All good, nothing to do here
+        })
+        .fail((jqXHR, textStatus, errorThrown) => {
+            console.error("jqXHR:", jqXHR);
+            console.error("Text Status:", textStatus);
+            console.error("Error Thrown:", errorThrown);
+            $(location).attr("href", "pages-login.html&message=" + encodeURI(testStatus));
+        });
 }
 
-isLoggedIn();
+function updateTokens(data) {
+    // Id token
+    var idToken = localStorage.getItem("idToken");
+    idToken.jwtToken = data.idToken;
+    localStorage.setItem("idToken", JSON.stringify(idToken));
+
+    // Access token
+    var accessToken = localStorage.getItem("accessToken");
+    accessToken.jwtToken = data.accessToken;
+    localStorage.setItem("accessToken", JSON.stringify(accessToken));
+}
 
 $("#logout").on("click", () => {
-    if (cognitoUser != null) {
-        cognitoUser.signOut();
-        $(location).attr("href", "pages-login.html");
-    }
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("idToken");
+    localStorage.removeItem("refreshToken");
+    localStorage.removeItem("clockDrift");
+    $(location).attr("href", "pages-login.html");
 });
+
+function showError(message) {
+    $("#error").html(message);
+    $("#error").show();
+}
